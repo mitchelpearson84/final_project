ae26c7af8eb0f69295298e2e73972cbe23fa7fd8
Mitchel Pearson <111671293+mitchelpearson84@users.noreply.github.com>
2023-09-07 11:15:52 -0500

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,11 @@
 URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-Input json: { "raw_document": { "text": text_to_analyse } }
+Input json: { "raw_document": { "text": "I love this new technology" } }
+{
+'anger': anger_score,
+'disgust': disgust_score,
+'fear': fear_score,
+'joy': joy_score,
+'sadness': sadness_score,
+'dominant_emotion': '<I am so happy doing this>'
+}
